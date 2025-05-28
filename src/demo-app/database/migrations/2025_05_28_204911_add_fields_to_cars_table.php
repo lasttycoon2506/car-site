cc36b38553b25467c70fcbd65ab2ecfcbceef58e
William Roberts <werobert@yahoo.com>
2025-05-28 17:02:53 -0400

    cars-table fields

---
+++ src/demo-app/database/migrations/2025_05_28_204911_add_fields_to_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->tinyText("make");
+            $table->tinyText("model");
+            $table->tinyText("condition");
+            $table->tinyText("transmission");
+            $table->tinyText("drive_type");
+            $table->unsignedTinyInteger("year");
+            $table->unsignedTinyInteger("mpg");
+            $table->unsignedInteger("miles");
+            $table->unsignedInteger("price");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropColumns("cars", ["make", "model", "condition", "transmission", "drive_type", "year", "mpg", "miles", "price"]);
+    }
+};
