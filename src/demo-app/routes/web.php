07dedf9d56685eed3cc9b56bc16e27c4e19b751c
William Roberts <werobert@yahoo.com>
2025-05-29 11:33:40 -0400

    car routes

--- src/demo-app/routes/web.php
+++ src/demo-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, "index"]);
 Route::get('/show', [IndexController::class, "show"]);
+
+Route::resource("car", CarController::class);
