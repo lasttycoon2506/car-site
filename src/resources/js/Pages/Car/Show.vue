053c938096dce7cb9d9ec8db69d4753dd0f1ca6c
William Roberts <werobert@yahoo.com>
2025-06-09 16:29:11 -0400

    details pg refactor

--- src/resources/js/Pages/Car/Show.vue
+++ src/resources/js/Pages/Car/Show.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="grid grid-cols-3 gap-5 px-10 py-3">
-        <div class="grid col bg-sky-300 rounded-lg px-5 py-2">
-            <span class="font-serif text-2xl">{{ car.make }}</span>
-            <span class="font-serif text-2xl">{{ car.model }}</span>
-            <span class="font-serif text-2xl">{{ car.year }}</span>
-            <span class="font-serif text-2xl">{{ car.condition }}</span>
-            <span class="font-serif text-2xl">{{ car.drive_type }}</span>
-            <span class="font-serif text-2xl">{{ car.miles.toLocaleString() }}</span>
-            <span class="font-serif text-2xl">{{ car.mpg }}</span>
-            <span class="font-serif text-2xl">
+    <div class="grid grid-cols-3 gap-5 px-10 py-10">
+        <div
+            class="grid col bg-sky-300/50 rounded-lg px-5 py-2 me-10 border-4 bg-clip-padding shadow-xl font-serif text-2xl font-bold">
+            <span>{{ car.make }}</span>
+            <span>{{ car.model }}</span>
+            <span>{{ car.year }}</span>
+            <span>{{ car.condition }}</span>
+            <span>{{ car.drive_type }}</span>
+            <span>{{ car.miles.toLocaleString() }}</span>
+            <span>{{ car.mpg }} mpg</span>
+            <span>
                 <PriceFormatter :price="car.price"></PriceFormatter>
             </span>
-            <span class="font-serif text-2xl">{{ car.transmission }}</span>
+            <span>{{ car.transmission }}</span>
         </div>
         <div class="col-span-2">
-            <img :src="mainImage" class="rounded-lg max-h-80 mb-4" />
+            <img :src="mainImage" class="rounded-xl border border-black max-h-80 mb-4" />
             <div class="flex gap-2">
                 <img v-for="(pic, idx) in car.pictures" :key="idx" :src="pic"
                     class="rounded cursor-pointer w-20 h-20 object-cover border-2"
