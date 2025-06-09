7cdf58f744d9eb20f9eec1fa9ccfa0b5dd433fcc
William Roberts <werobert@yahoo.com>
2025-06-09 17:14:50 -0400

    removed unused index route

--- src/resources/js/Pages/Index/Show.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>show me the $$$$$$$$$$$$</div>
-</template>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\CarController;
-use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, "index"]);
-Route::get('/show', [IndexController::class, "show"]);
 
 Route::resource("car", CarController::class);
