31b044f00ea7f14ddd271e3b335939a806db0cb1
William Roberts <werobert@yahoo.com>
2025-06-10 14:37:14 -0400

    edit car error msgs

--- src/resources/js/Pages/Car/Edit.vue
+++ src/resources/js/Pages/Car/Edit.vue
@@ -5,47 +5,47 @@
             <div class="mx-12">
                 <label class="label">Make</label>
                 <input v-model="carForm.make" type="text" class="input">
-                <div v-if="carForm.errors.make">{{ carForm.errors.make }}</div>
+                <div v-if="carForm.errors.make" class="input-error">{{ carForm.errors.make }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Model</label>
                 <input v-model="carForm.model" type="text" class="input">
-                <div v-if="carForm.errors.model">{{ carForm.errors.model }}</div>
+                <div v-if="carForm.errors.model" class="input-error">{{ carForm.errors.model }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Condition</label>
                 <input v-model="carForm.condition" type="text" class="input">
-                <div v-if="carForm.errors.condition">{{ carForm.errors.condition }}</div>
+                <div v-if="carForm.errors.condition" class="input-error">{{ carForm.errors.condition }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Transmission</label>
                 <input v-model="carForm.transmission" type="text" class="input">
-                <div v-if="carForm.errors.transmission">{{ carForm.errors.transmission }}</div>
+                <div v-if="carForm.errors.transmission" class="input-error">{{ carForm.errors.transmission }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Drive Type</label>
                 <input v-model="carForm.drive_type" type="text" class="input">
-                <div v-if="carForm.errors.drive_type">{{ carForm.errors.drive_type }}</div>
+                <div v-if="carForm.errors.drive_type" class="input-error">{{ carForm.errors.drive_type }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Year</label>
                 <input v-model="carForm.year" type="text">
-                <div v-if="carForm.errors.year">{{ carForm.errors.year }}</div>
+                <div v-if="carForm.errors.year" class="input-error">{{ carForm.errors.year }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">MPG</label>
                 <input v-model="carForm.mpg" type="text">
-                <div v-if="carForm.errors.mpg">{{ carForm.errors.mpg }}</div>
+                <div v-if="carForm.errors.mpg" class="input-error">{{ carForm.errors.mpg }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Mi.</label>
                 <input v-model="carForm.miles" type="text">
-                <div v-if="carForm.errors.miles">{{ carForm.errors.miles }}</div>
+                <div v-if="carForm.errors.miles" class="input-error">{{ carForm.errors.miles }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Price</label>
                 <input v-model="carForm.price" type="text">
-                <div v-if="carForm.errors.price">{{ carForm.errors.price }}</div>
+                <div v-if="carForm.errors.price" class="input-error">{{ carForm.errors.price }}</div>
             </div>
             <div class="mx-12">
                 <label class="label">Pictures</label>
