c7089c45e98707ba3ce8a0de9b08137a139f01c2
William Roberts <werobert@yahoo.com>
2025-06-10 12:59:15 -0400

    extract label class

--- src/resources/js/Pages/Car/Create.vue
+++ src/resources/js/Pages/Car/Create.vue
@@ -1,52 +1,54 @@
 <template>
     <form @submit.prevent="createCar">
-        <div>
-            <label>Make</label>
-            <input v-model="carForm.make" type="text">
-            <div v-if="carForm.errors.make">{{ carForm.errors.make }}</div>
-        </div>
-        <div>
-            <label>Model</label>
-            <input v-model="carForm.model" type="text">
-            <div v-if="carForm.errors.model">{{ carForm.errors.model }}</div>
-        </div>
-        <div>
-            <label>Condition</label>
-            <input v-model="carForm.condition" type="text">
-            <div v-if="carForm.errors.condition">{{ carForm.errors.condition }}</div>
-        </div>
-        <div>
-            <label>Transmission</label>
-            <input v-model="carForm.transmission" type="text">
-            <div v-if="carForm.errors.transmission">{{ carForm.errors.transmission }}</div>
-        </div>
-        <div>
-            <label>Drive Type</label>
-            <input v-model="carForm.drive_type" type="text">
-            <div v-if="carForm.errors.drive_type">{{ carForm.errors.drive_type }}</div>
-        </div>
-        <div>
-            <label>Year</label>
-            <input v-model="carForm.year" type="text">
-            <div v-if="carForm.errors.year">{{ carForm.errors.year }}</div>
-        </div>
-        <div>
-            <label>MPG</label>
-            <input v-model="carForm.mpg" type="text">
-            <div v-if="carForm.errors.mpg">{{ carForm.errors.mpg }}</div>
-        </div>
-        <div>
-            <label>Mi.</label>
-            <input v-model="carForm.miles" type="text">
-            <div v-if="carForm.errors.miles">{{ carForm.errors.miles }}</div>
-        </div>
-        <div>
-            <label>Price</label>
-            <input v-model="carForm.price" type="text">
-            <div v-if="carForm.errors.price">{{ carForm.errors.price }}</div>
-        </div>
-        <div>
-            <button type="submit">create </button>
+        <div class="grid grid-cols-2 px-12 py-10 bg-slate-400">
+            <div>
+                <label class="label">Make</label>
+                <input v-model="carForm.make" type="text">
+                <div v-if="carForm.errors.make">{{ carForm.errors.make }}</div>
+            </div>
+            <div>
+                <label class="label">Model</label>
+                <input v-model="carForm.model" type="text">
+                <div v-if="carForm.errors.model">{{ carForm.errors.model }}</div>
+            </div>
+            <div>
+                <label class="label">Condition</label>
+                <input v-model="carForm.condition" type="text">
+                <div v-if="carForm.errors.condition">{{ carForm.errors.condition }}</div>
+            </div>
+            <div>
+                <label class="label">Transmission</label>
+                <input v-model="carForm.transmission" type="text">
+                <div v-if="carForm.errors.transmission">{{ carForm.errors.transmission }}</div>
+            </div>
+            <div>
+                <label class="label">Drive Type</label>
+                <input v-model="carForm.drive_type" type="text">
+                <div v-if="carForm.errors.drive_type">{{ carForm.errors.drive_type }}</div>
+            </div>
+            <div>
+                <label class="label">Year</label>
+                <input v-model="carForm.year" type="text">
+                <div v-if="carForm.errors.year">{{ carForm.errors.year }}</div>
+            </div>
+            <div>
+                <label class="label">MPG</label>
+                <input v-model="carForm.mpg" type="text">
+                <div v-if="carForm.errors.mpg">{{ carForm.errors.mpg }}</div>
+            </div>
+            <div>
+                <label class="label">Mi.</label>
+                <input v-model="carForm.miles" type="text">
+                <div v-if="carForm.errors.miles">{{ carForm.errors.miles }}</div>
+            </div>
+            <div>
+                <label class="label">Price</label>
+                <input v-model="carForm.price" type="text">
+                <div v-if="carForm.errors.price">{{ carForm.errors.price }}</div>
+            </div>
+            <div>
+                <button type="submit">Sell </button>
+            </div>
         </div>
     </form>
 </template>
