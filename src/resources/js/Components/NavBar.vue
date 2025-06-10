a30ee7c9139f67fa0dead09f95aaf167cf940899
William Roberts <werobert@yahoo.com>
2025-06-10 12:37:40 -0400

    sell btn styhling

--- src/resources/js/Components/NavBar.vue
+++ src/resources/js/Components/NavBar.vue
@@ -1,19 +1,22 @@
 <template>
     <header class="items-center rounded-lg border-b bg-slate-600">
-        <div class="flex justify-start items-center">
-            <Link href="/car">
-            <div class="h-16 w-16 bg-cover bg-center rounded-[2vw] bg-[url('/mustang.jpg')] px-10 me-3 my-1 ms-1">
+        <div class="flex justify-between">
+            <div class="flex items-center">
+                <Link href="/car">
+                <div class="h-16 w-16 bg-cover bg-center rounded-[2vw] bg-[url('/mustang.jpg')] px-10 me-3 my-1 ms-1">
+                </div>
+                </Link>
+                <div class="shadow-xl font-bold shadow-white text-xl text-red-600">
+                    <Link href="/car" class="bg-slate-600"> Carzies
+                    </Link>
+                </div>
             </div>
-            </Link>
-            <div class="shadow-xl font-bold shadow-white text-xl text-red-600">
-                <Link href="/car" class="bg-slate-600"> Carzies
+            <div class="flex justify-end items-center me-5">
+                <Link href="/car/create"> <button class="rounded-full bg-slate-200 py-2 px-6 font-bold">Sell</button>
                 </Link>
             </div>
         </div>
-        <div class="flex justify-end">
-            <Link href="/car/create"> <button class="rounded-full bg-slate-200 py-1 px-1">Sell</button>
-            </Link>
-        </div>
+
     </header>
 </template>
 
