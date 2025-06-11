12396b2b4a78c8c96f4c629b49f50a054c29860d
William Roberts <werobert@yahoo.com>
2025-06-11 10:35:07 -0400

    cars-card styling

--- src/resources/js/Pages/Car/Index.vue
+++ src/resources/js/Pages/Car/Index.vue
@@ -8,23 +8,35 @@
                 <div>
                     <Link :href="`/car/${car.id}`">
                     <img :src="`${car.pictures[0]}`" class="rounded-lg border border-black">
-                    <span class="font-bold">{{ car.make }}</span> <span class="text-gray-500">| </span>
-                    <span class="font-bold">{{ car.model }}</span> <span class="text-gray-500">| </span>
-                    <span class="font-bold">{{ car.year }}</span> <span class="text-gray-500">| </span>
-                    <span>Condition: </span><span class="font-bold">{{ car.condition }}</span> <span
-                        class="text-gray-500">| </span>
-                    <span class="font-bold">{{ car.drive_type }}</span> <span class="text-gray-500">| </span>
-                    <span class="font-bold">{{ car.miles.toLocaleString() }}</span> mi. <span class="text-gray-500">|
-                    </span>
-                    <span class="font-bold">{{ car.mpg }}</span> mpg <span class="text-gray-500">| </span>
-                    <span class="font-bold">
-                        <PriceFormatter :price=car.price></PriceFormatter>
-                    </span><span class="text-gray-500"> | </span>
-                    <span class="font-bold">{{ car.transmission }}</span>
+                    <div class="flex justify-evenly">
+                        <span class="font-bold">{{ car.make }}</span> <span class="text-gray-500">| </span>
+                        <span class="font-bold">{{ car.model }}</span> <span class="text-gray-500">| </span>
+                        <span class="font-bold">{{ car.year }}</span>
+                    </div>
+                    <div class="flex justify-evenly">
+                        <div>
+                            <span>Condition: </span><span class="font-bold">{{ car.condition }}</span>
+                        </div>
+                        <span class="text-gray-500">| </span>
+                        <div>
+                            <span>Drive: </span><span class="font-bold">{{ car.drive_type }}</span>
+                        </div>
+                    </div>
+                    <div class="flex justify-evenly">
+                        <span class="font-bold">{{ car.miles.toLocaleString() }}</span> mi. <span
+                            class="text-gray-500">|
+                        </span>
+                        <span class="font-bold">{{ car.mpg }}</span> mpg <span class="text-gray-500">| </span>
+                        <span class="font-bold">{{ car.transmission }}</span>
+                    </div>
+                    <div class="flex justify-center"><span class="font-bold">
+                            <PriceFormatter :price=car.price></PriceFormatter>
+                        </span><span class="text-gray-500"> &nbsp or &nbsp</span>
+                        <MonthlyPayment :car="car" />
+                        <span> &nbsp/ month</span>
+                    </div>
                     </Link>
                 </div>
-                <MonthlyPayment :car="car" />
-                <span> / month</span>
                 <!-- <div>
                     <Link :href="`/car/${car.id}/edit`">
                     Edit
