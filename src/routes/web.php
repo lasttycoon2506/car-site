14dd470dd74413076a3890d77d9c84f12f13fd9e
William Roberts <werobert@yahoo.com>
2025-06-11 11:54:00 -0400

    update routes auth-controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::resource("car", CarController::class);
+
+Route::get("login", [AuthController::class, "create"])->name("login");
+Route::post("login", [AuthController::class, "store"])->name("login.store");
+Route::delete("logout", [AuthController::class, "destroy"])->name("logout");
