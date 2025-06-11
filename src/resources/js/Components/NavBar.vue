21d88851a8bb1bf4114dc7a5ce40fbe4e9516206
William Roberts <werobert@yahoo.com>
2025-06-11 16:52:04 -0400

    protected routes

--- src/resources/js/Components/NavBar.vue
+++ src/resources/js/Components/NavBar.vue
@@ -11,12 +11,12 @@
                     </Link>
                 </div>
             </div>
-            <div v-if="user" class="flex justify-end items-center gap-4">
+            <div v-if="user" class="flex justify-end items-center gap-6">
                 <div class="text-md text-rose-600">{{ user.email }}</div>
                 <Link href="/car/create"> <button class="rounded-lg bg-lime-500 py-2 px-6 font-bold">Sell</button>
                 </Link>
                 <Link href="/logout" method="delete"> <button
-                    class="rounded-lg bg-red-700 py-2 px-6 font-bold me-4">Logout</button>
+                    class="rounded-lg bg-red-700 py-2 px-6 font-bold me-6">Logout</button>
                 </Link>
             </div>
             <div v-else class="flex items-center me-10">
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,11 @@ use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::resource("car", CarController::class);
+Route::resource("car", CarController::class)
+    ->only("create", "store", "edit", "update", "destroy")
+    ->middleware("auth");
+Route::resource("car", CarController::class)
+    ->except("create", "store", "edit", "update", "destroy");
 
 Route::get("login", [AuthController::class, "create"])->name("login");
 Route::post("login", [AuthController::class, "store"])->name("login.store");
