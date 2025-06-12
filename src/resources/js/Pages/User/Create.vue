4ca25b9cf027ae523dc1556d99bb690cc9bba822
William Roberts <werobert@yahoo.com>
2025-06-12 11:57:34 -0400

    newUser pg styling

--- src/resources/js/Pages/User/Create.vue
+++ src/resources/js/Pages/User/Create.vue
@@ -1,22 +1,31 @@
 <template>
     <form @submit.prevent="createUser">
         <div class="w-1/2 mx-auto border border-black mt-10 rounded bg-blue-300">
-            <div class="flex justify-center">
+            <div class="w-1/2 mx-auto">
                 <div class="mt-10">
-                    <label class="w-full font-semibold">Email</label>
+                    <label class="font-semibold">Name</label>
                     <input v-model="newUserForm.email" class="w-full shadow-lg mt-1 rounded" type="text">
                     <div v-if="newUserForm.errors.email">{{ newUserForm.errors.email }}</div>
                 </div>
-            </div>
-            <div class="flex justify-center">
                 <div class="mt-5">
-                    <label class="w-full font-semibold">Password</label>
+                    <label class="font-semibold">Email</label>
+                    <input v-model="newUserForm.email" class="w-full shadow-lg mt-1 rounded" type="text">
+                    <div v-if="newUserForm.errors.email">{{ newUserForm.errors.email }}</div>
+                </div>
+                <div class="mt-5">
+                    <label class="font-semibold">Password</label>
+                    <input v-model="newUserForm.password" class="w-full shadow-lg mt-1 rounded" type="password">
+                    <div v-if="newUserForm.errors.email">{{ newUserForm.errors.password }}</div>
+                </div>
+                <div class="mt-5">
+                    <label class="font-semibold">Confirm Password</label>
                     <input v-model="newUserForm.password" class="w-full shadow-lg mt-1 rounded" type="password">
                     <div v-if="newUserForm.errors.email">{{ newUserForm.errors.password }}</div>
                 </div>
             </div>
+
             <div class="flex justify-center">
-                <button type="submit" class="rounded-full bg-amber-600 py-3 px-12 mt-10 mb-5 font-semibold">Login
+                <button type="submit" class="rounded-full bg-amber-600 py-3 px-12 mt-10 mb-5 font-semibold">Join
                 </button>
             </div>
         </div>
