f289cbac8c2b94ba498acda373e814aff4f16bf6
William Roberts <werobert@yahoo.com>
2025-06-12 12:09:30 -0400

    newUser store in db

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -11,5 +13,20 @@ class UserController extends Controller
         return inertia(
             "User/Create"
         );
+    }
+
+    public function store(Request $request)
+    {
+        $newUser = User::make($request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "condition" => "required|min:8|confirmed",
+        ]));
+
+        $newUser->save();
+
+        Auth::login($newUser);
+
+        return redirect()->route("car.index")->with("success", "Account Created, Welcome!");
     }
 }
