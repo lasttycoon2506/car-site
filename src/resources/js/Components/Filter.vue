d8b3b537a5976d6f574ccc0649228daf957f8105
William Roberts <werobert@yahoo.com>
2025-06-13 11:18:11 -0400

    selected on create-car dropdowns

--- src/resources/js/Components/Filter.vue
+++ src/resources/js/Components/Filter.vue
@@ -1,24 +1,33 @@
 <template>
     <form action="">
-        <div class="flex justify-center">
-            <select class="w-1/2">
-                <option disabled>Condition</option>
+        <div class="flex justify-start gap-5">
+            <select>
+                <option disabled selected>Condition</option>
                 <option value="Ok">Ok</option>
                 <option value="Good">Good</option>
                 <option value="Great">Great</option>
                 <option value="Excellent">Excellent</option>
                 <option value="Like New">Like New</option>
             </select>
-            <div>price</div>
-            <select class="w-1/2">
-                <option disabled>Transmission</option>
+            <input class="w-1/12" type="text" placeholder="price from">
+            <input class="w-1/12" type="text" placeholder="price to">
+            <select>
+                <option disabled selected>Transmission</option>
                 <option value="Automatic">Automatic</option>
                 <option value="Manual">Manual</option>
             </select>
-            <div>drive type</div>
+            <select>
+                <option disabled>Select</option>
+                <option value="FWD">FWD</option>
+                <option value="RWD">RWD</option>
+                <option value="AWD">AWD</option>
+                <option value="4WD">4WD</option>
+            </select>
             <div>year</div>
             <div>mpg</div>
             <div>mi</div>
+            <button type="submit">filter</button>
+            <button type="reset">reset</button>
         </div>
     </form>
 </template>
