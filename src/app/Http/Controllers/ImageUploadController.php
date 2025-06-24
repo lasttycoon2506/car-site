41f75d5cfeb7d1be674e5ee6fb9708fcdd5441a8
William Roberts <werobert@yahoo.com>
2025-06-24 17:02:48 -0400

    cloudinary api store pic

--- src/app/Http/Controllers/ImageUploadController.php
+++ src/app/Http/Controllers/ImageUploadController.php
@@ -19,37 +19,6 @@ class ImageUploadController extends Controller
 
     public function store(Request $request)
     {
-        // Configure an instance of your Cloudinary cloud
-        // Configuration::instance(env("CLOUDINARY_URL"));
-
-        // Use the UploadApi class for uploading assets
-        // $upload = new UploadApi();
-        // echo '<pre>';
-        // if ($request->isMethod('post') && $request->hasFile('image')) {
-        //     $file = $request->file('image')->getRealPath();
-        //     try {
-        //         echo json_encode(
-        //             $upload->upload($file, [
-        //                 'public_id' => 'flower_sample',
-        //                 'use_filename' => true,
-        //                 'overwrite' => true
-        //             ]),
-        //             JSON_PRETTY_PRINT
-        //         );
-        //         echo '</pre>';
-        //     } catch (\Exception $e) {
-        //         echo 'Upload failed: ' . $e->getMessage();
-        //     }
-        // }
-        // $request->validate([
-        //     'image' => 'required|image|max:5120', // 5MB max
-        // ]);
-        // // Upload to Cloudinary
-        // $cloudinary = new Cloudinary(env("CLOUDINARY_URL"));
-        // $uploadResult = $cloudinary->uploadApi()->upload($request->file('image')->getRealPath());
-        // $uploadedFileUrl = $uploadResult['secure_url'] ?? null;
-
-        // return response()->json(['url' => $uploadedFileUrl]);
         if ($request->hasFile("images")) {
             $cloudinary = new Cloudinary(env("CLOUDINARY_URL"));
             foreach ($request->file("images") as $file) {
@@ -57,6 +26,5 @@ class ImageUploadController extends Controller
                 $uploadedFileUrl = $uploadResult['secure_url'] ?? null;
             }
         }
-        dd($request);
     }
 }
--- src/resources/js/Pages/Seller/CarImage/Create.vue
+++ src/resources/js/Pages/Seller/CarImage/Create.vue
@@ -1,7 +1,7 @@
 <template>
     <form method="POST" :action="`/seller/car/${car.id}/image`" enctype="multipart/form-data">
         <input type="hidden" name="_token" :value="csrf" />
-        <input type="file" name="files[]" multiple>
+        <input type="file" name="images[]" multiple>
         <button type="submit">submit</button>
     </form>
 </template>
