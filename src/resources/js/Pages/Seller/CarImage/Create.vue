1864568848dc9696486bec357776d09abe192124
William Roberts <werobert@yahoo.com>
2025-06-24 16:00:10 -0400

    csrf form

--- src/resources/js/Pages/Seller/CarImage/Create.vue
+++ src/resources/js/Pages/Seller/CarImage/Create.vue
@@ -1,6 +1,6 @@
 <template>
     <form method="post" :action="`/seller/car/${car.id}/image`">
-        <input type="file" name="files" multiple>
+        <input type="file" name="files" multiple :value="csrf">
         <button type="submit">submit</button>
     </form>
 </template>
@@ -8,5 +8,6 @@
 <script setup lang="ts">
 import { Car } from '@/resources/types/car';
 
+const csrf = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') ?? '';
 defineProps<{ car: Car }>()
 </script>
