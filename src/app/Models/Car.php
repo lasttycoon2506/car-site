7c588261b9c40bb0c17256c60c7f1ebdd8eee614
William Roberts <werobert@yahoo.com>
2025-06-24 12:56:55 -0400

    car model upd.

--- src/app/Models/Car.php
+++ src/app/Models/Car.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Car extends Model
 {
@@ -31,6 +32,11 @@ class Car extends Model
     public function scopeMostRecent(Builder $query): Builder
     {
         return $query->orderByDesc("created_at");
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(CarImage::class);
     }
 
     public function scopeFilter(Builder $query, array $filters): Builder
