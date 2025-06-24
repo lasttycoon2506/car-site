5f9df88b8d82bca03f56cad6f22e543302798b4c
William Roberts <werobert@yahoo.com>
2025-06-24 15:28:38 -0400

    img-upload template-more

--- src/resources/js/Pages/Seller/CarImage/Create.vue
+++ src/resources/js/Pages/Seller/CarImage/Create.vue
@@ -1,1 +1,12 @@
-<template>chacha</template>
+<template>
+    <form method="post" :action="`/seller/car/${car.id}/image`">
+        <input type="file" name="files" multiple>
+        <button type="submit">submit</button>
+    </form>
+</template>
+
+<script setup lang="ts">
+import { Car } from '@/resources/types/car';
+
+defineProps<{ car: Car }>()
+</script>
