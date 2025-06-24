9777845aede74a5b89f8307da734ef390436d1bb
William Roberts <werobert@yahoo.com>
2025-06-24 12:33:46 -0400

    carImages migration

---
+++ src/database/migrations/2025_06_24_162743_create_car_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Car;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('car_images', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string("file");
+
+            $table->foreignIdFor(Car::class)->constrained("cars");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('car_images');
+    }
+};
