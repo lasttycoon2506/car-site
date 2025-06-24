61ff14404bf2763b0db1efdf6a833beeaffc08ac
William Roberts <werobert@yahoo.com>
2025-06-24 12:45:04 -0400

    carImg model

--- src/app/Models/CarImage.php
+++ src/app/Models/CarImage.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CarImage extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "file"
+    ];
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
 }
