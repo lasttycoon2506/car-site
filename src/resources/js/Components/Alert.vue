25b86bcb5c1655ff8a866f191c2fbf08d6866078
William Roberts <werobert@yahoo.com>
2025-06-25 15:07:20 -0400

    removed regex alert component

--- src/resources/js/Components/Alert.vue
+++ src/resources/js/Components/Alert.vue
@@ -1,11 +1,7 @@
 <template>
-    <div v-if="success.match('car edited!')" class="bg-green-300 rounded-lg border-green-300 shadow-lg ps-5">{{ success
-        }}</div>
-    <div v-if="success.match('car deleted!')" class="bg-red-600 rounded-lg border border-black shadow-lg ps-5">{{
-        success }}</div>
-    <div v-if="success.match('image(s) uploaded!')" class="bg-green-300 rounded-lg border-green-300 shadow-lg ps-5">{{
+    <div class="bg-green-300 rounded-lg border-green-300 shadow-lg ps-5 py-1">{{
         success
-        }}</div>
+    }}</div>
 </template>
 
 <script setup lang="ts">
