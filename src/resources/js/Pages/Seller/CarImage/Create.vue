215ef81a1a29ebca4ac89b2b2524db434f311a49
William Roberts <werobert@yahoo.com>
2025-06-25 11:31:25 -0400

    uploadImg success notif

--- src/resources/js/Pages/Seller/CarImage/Create.vue
+++ src/resources/js/Pages/Seller/CarImage/Create.vue
@@ -1,4 +1,7 @@
 <template>
+    <div v-if="alertMsg">
+        <Alert :success="alertMsg" />
+    </div>
     <form @submit.prevent="uploadImages">
         <input type="hidden" name="_token" :value="csrf" />
         <input type="file" multiple @input="addFiles">
@@ -8,13 +11,21 @@
 </template>
 
 <script setup lang="ts">
+import Alert from '@/resources/js/Components/Alert.vue';
 import { Car } from '@/resources/types/car';
-import { InertiaForm, useForm } from '@inertiajs/vue3';
+import { InertiaForm, useForm, usePage } from '@inertiajs/vue3';
+import { computed, ComputedRef } from 'vue';
+
+type PageProps = {
+    flash: { success: string },
+}
 
 const csrf: string = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') ?? '';
 const props = defineProps<{ car: Car }>()
 const imageForm: InertiaForm<{ images: File[]; }> =
     useForm<{ images: File[] }>({ images: [] })
+const page = usePage<PageProps>()
+const alertMsg: ComputedRef<string> = computed(() => page.props.flash?.success)
 
 const uploadImages: () => void =
     () => imageForm.post(
