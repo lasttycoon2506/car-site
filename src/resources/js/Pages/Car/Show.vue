57f5962bb198d3926aee2f8a01bb43e13e32a977
William Roberts <werobert@yahoo.com>
2025-06-26 14:34:00 -0400

    car-details pg formatting

--- src/resources/js/Pages/Car/Show.vue
+++ src/resources/js/Pages/Car/Show.vue
@@ -3,31 +3,31 @@
         <div
             class="grid col bg-sky-300/50 rounded-lg px-5 py-3 me-12 border border-black bg-clip-padding shadow-xl font-serif text-2xl font-bold">
             <div>
-                <span class="font-normal">Make: </span><span> {{ car.make }}</span>
+                <span class="font-normal text-xl">Make: </span><span> {{ car.make }}</span>
             </div>
             <div>
-                <span class="font-normal">Model: </span><span> {{ car.model }}</span>
+                <span class="font-normal text-xl">Model: </span><span> {{ car.model }}</span>
             </div>
             <div>
-                <span class="font-normal">Year: </span><span> {{ car.year }}</span>
+                <span class="font-normal text-xl">Year: </span><span> {{ car.year }}</span>
             </div>
             <div>
-                <span class="font-normal">Condition: </span><span> {{ car.condition }}</span>
+                <span class="font-normal text-xl">Condition: </span><span> {{ car.condition }}</span>
             </div>
             <div>
-                <span class="font-normal">Drive-Type: </span><span> {{ car.drive_type }}</span>
+                <span class="font-normal text-xl">Drive-Type: </span><span> {{ car.drive_type }}</span>
             </div>
             <div>
-                <span class="font-normal">Miles: </span><span> {{ car.miles.toLocaleString() }}</span>
+                <span class="font-normal text-xl">Miles: </span><span> {{ car.miles.toLocaleString() }}</span>
             </div>
             <div>
-                <span class="font-normal">MPG: </span><span> {{ car.mpg }}</span>
+                <span class="font-normal text-xl">MPG: </span><span> {{ car.mpg }}</span>
             </div>
             <span>
                 <PriceFormatter :price="car.price"></PriceFormatter>
             </span>
             <div>
-                <span class="font-normal">Transmission: </span><span> {{ car.transmission }}</span>
+                <span class="font-normal text-xl">Transmission: </span><span> {{ car.transmission }}</span>
             </div>
         </div>
         <div class="col-span-2">
