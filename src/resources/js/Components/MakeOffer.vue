e044404d5acd2c1d5c96d96074e7b5c0be8007c3
William Roberts <werobert@yahoo.com>
2025-06-27 12:51:22 -0400

    min/max offer offer-component

--- src/resources/js/Components/MakeOffer.vue
+++ src/resources/js/Components/MakeOffer.vue
@@ -1,17 +1,22 @@
 <template>
     <form>
         <input v-model.number="props.price" type="text">
-        <input v-model.number="props.price" type="range" min=".1" max="25" step=".1" class="cursor-pointer">
+        <input v-model.number="props.price" type="range" :min="minOffer" :max="maxOffer" step="1000"
+            class="cursor-pointer">
     </form>
 </template>
 
 <script setup lang="ts">
+import { computed, ComputedRef } from 'vue';
+
 
 const props = defineProps<{
     carId: number,
     price: number,
 }>()
 
+const minOffer: ComputedRef<number> = computed(() => props.price / 2)
+const maxOffer: ComputedRef<number> = computed(() => props.price * 2)
 
 
 </script>
