52f077e6172d665ec70282e610824ef4afedadb3
William Roberts <werobert@yahoo.com>
2025-06-27 12:48:50 -0400

    makeOffer component

---
+++ src/resources/js/Components/MakeOffer.vue
@@ -0,0 +1,17 @@
+<template>
+    <form>
+        <input v-model.number="props.price" type="text">
+        <input v-model.number="props.price" type="range" min=".1" max="25" step=".1" class="cursor-pointer">
+    </form>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    carId: number,
+    price: number,
+}>()
+
+
+
+</script>
