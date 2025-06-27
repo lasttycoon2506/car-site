36abf19573224ee73b1b64fa927cb0b557e3a560
William Roberts <werobert@yahoo.com>
2025-06-27 12:11:47 -0400

    offer model

--- src/app/Models/Offer.php
+++ src/app/Models/Offer.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Offer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "amount",
+        "accepted_at",
+        "declined_at"
+    ];
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class, "car_id");
+    }
+
+    public function bidder(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "bidder_id");
+    }
 }
