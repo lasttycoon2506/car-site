47bdf41480ba87d799f1d80faf2b27d848c676e2
William Roberts <werobert@yahoo.com>
2025-06-30 16:48:52 -0400

    offer-accepted display

--- src/resources/js/Components/OfferCard.vue
+++ src/resources/js/Components/OfferCard.vue
@@ -2,11 +2,13 @@
     <div class="outline rounded-md px-2 py-2">
         <div class="flex justify-around">
             <div>
+                <div v-if="offer.accepted_at" class="bg-green-500 text-center px-1 py-1 rounded-md font-semibold">
+                    Accepted!</div>
                 <PriceFormatter :price="offer.amount" :allow-negative="false" class="text-xl"></PriceFormatter>
                 <div>Made By: {{ offer.bidder.name }}</div>
                 <div class="text-gray-500">Made: {{ offerMadeOn }}</div>
             </div>
-            <div class="flex items-center">
+            <div v-if="!offer.declined_at && !offer.accepted_at" class="flex items-center">
                 <Link :href="`/seller/offer/${offer.id}/accept`" method="put"
                     class="rounded-lg bg-amber-400 py-2 px-10 font-semibold hover:bg-amber-800">
                 Accept
@@ -23,20 +25,11 @@ import { computed, ComputedRef } from 'vue';
 import { Link } from '@inertiajs/vue3';
 
 
-
-// id: number;
-// accepted_at: string;
-// amount: number;
-// bidder_id: number;
-// car_id: number;
-// created_at: string;
-// declined_at: string;
-// updated_at: string;
-
-
 const props = defineProps<{
     offer: OfferMade,
 }>()
+
+console.log(props.offer)
 
 const offerMadeOn: ComputedRef<string> = computed(() => new Date(props.offer.created_at).toLocaleDateString())
 </script>
