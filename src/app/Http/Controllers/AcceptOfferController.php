653488ca2dce246a06022464772d0d5814fbf9c1
William Roberts <werobert@yahoo.com>
2025-07-01 12:32:50 -0400

    unused imports

--- src/app/Http/Controllers/AcceptOfferController.php
+++ src/app/Http/Controllers/AcceptOfferController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Offer;
-use Illuminate\Http\Request;
 
 class AcceptOfferController extends Controller
 {
--- src/app/Policies/CarPolicy.php
+++ src/app/Policies/CarPolicy.php
@@ -5,7 +5,6 @@ namespace App\Policies;
 use App\Models\Car;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Auth\Access\Response;
 
 class CarPolicy
 {
