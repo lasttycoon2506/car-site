d8e7fdd0d1bea45bf1c2a585391cd0f01423800b
William Roberts <werobert@yahoo.com>
2025-07-01 16:57:12 -0400

    see car/offer btns

--- src/resources/js/Components/NotificationCard.vue
+++ src/resources/js/Components/NotificationCard.vue
@@ -4,8 +4,13 @@
         </PriceFormatter>
         for {{ notification.data.car_make }} {{ notification.data.car_model }}
         on {{ notifCreatedAt }}
-        <Link :href="`/cars/${notification.data.car_id}`">
-        Listing
+        <Link :href="`/cars/${notification.data.car_id}`"
+            class="rounded-lg bg-amber-400 py-2 px-3 font-semibold hover:bg-amber-800">
+        See Car
+        </Link>
+        <Link :href="`/seller/cars/${notification.data.car_id}`"
+            class="rounded-lg bg-amber-400 py-2 px-3 font-semibold hover:bg-amber-800">
+        See Offer
         </Link>
     </div>
 </template>
