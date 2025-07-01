3ee94379a9feaa682ba7deedce0de9ac50f56b73
William Roberts <werobert@yahoo.com>
2025-07-01 16:38:57 -0400

    notif type upd

--- src/resources/types/notification.ts
+++ src/resources/types/notification.ts
@@ -3,6 +3,8 @@ export type Notification = {
     created_at: string;
     data: {
         car_id: number;
+        make: string;
+        model: string;
         bidder_id: number;
         offer_id: number;
         amount: number;
