c815b9d8ccad66aa43c5cb71521dbbe0135886ce
William Roberts <werobert@yahoo.com>
2025-07-01 15:27:17 -0400

    backend routing upd

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,22 +7,37 @@ use App\Http\Controllers\OfferController;
 use App\Http\Controllers\SellerController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AcceptOfferController;
+use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::resource("cars", CarController::class)
     ->only("index", "show");
 
-Route::get("login", [AuthController::class, "create"])->name("login");
-Route::post("login", [AuthController::class, "store"])->name("login.store");
-Route::delete("logout", [AuthController::class, "destroy"])->name("logout");
+Route::get("login", [AuthController::class, "create"])
+    ->name("login");
+Route::post("login", [AuthController::class, "store"])
+    ->name("login.store");
+Route::delete("logout", [AuthController::class, "destroy"])
+    ->name("logout");
 
-Route::resource("user", UserController::class)->only(["create", "store"]);
+Route::resource("user", UserController::class)
+    ->only(["create", "store"]);
 
-Route::resource("car.offer", OfferController::class)->middleware("auth")->only(["store"]);
+Route::resource("car.offer", OfferController::class)
+    ->middleware("auth")
+    ->only(["store"]);
 
-Route::prefix("seller")->name("seller.")->middleware("auth")->group(function () {
-    Route::resource("/cars", SellerController::class);
-    Route::name("offer.accept")->put("/offer/{offer}/accept", AcceptOfferController::class);
-    Route::resource("car.image", ImageUploadController::class)->only("create", "store", "destroy");
-});
+Route::prefix("seller")->name("seller.")
+    ->middleware("auth")
+    ->group(function () {
+        Route::resource("/cars", SellerController::class);
+        Route::name("offer.accept")
+            ->put("/offer/{offer}/accept", AcceptOfferController::class);
+        Route::resource("car.image", ImageUploadController::class)
+            ->only("create", "store", "destroy");
+    });
+
+Route::resource("notifications", NotificationController::class)
+    ->middleware("auth")
+    ->only(["index"]);
