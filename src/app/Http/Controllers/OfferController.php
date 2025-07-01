fdcc224234352cbc57a7f3bc50b33f9d3942dde0
William Roberts <werobert@yahoo.com>
2025-07-01 13:40:31 -0400

    new notif class upon offer

--- src/app/Http/Controllers/OfferController.php
+++ src/app/Http/Controllers/OfferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use App\Models\Offer;
+use App\Notifications\OfferMade;
 use Illuminate\Http\Request;
 
 class OfferController extends Controller
@@ -12,10 +13,11 @@ class OfferController extends Controller
     {
         $this->authorize("view", $car);
 
-        $car->offers()->save(Offer::make(
+        $offer = $car->offers()->save(Offer::make(
             $request->validate(["amount" => "required|integer|min:1|max:500000"])
         )->bidder()->associate($request->user()));
 
+        $car->owner->notify(new OfferMade($offer));
 
         return redirect()->back()->with("success", "offer made!");
     }
