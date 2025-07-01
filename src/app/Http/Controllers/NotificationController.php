4e7dca8d1a00c310fd1e82efeb52327a53d985f9
William Roberts <werobert@yahoo.com>
2025-07-01 15:24:07 -0400

    notif controller

---
+++ src/app/Http/Controllers/NotificationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        return inertia(
+            "Notification/Index",
+            [
+                "notifications" =>
+                $request
+                    ->user()
+                    ->notifications()
+                    ->mostRecent()
+                    ->paginate(10)
+            ]
+        );
+    }
+}
