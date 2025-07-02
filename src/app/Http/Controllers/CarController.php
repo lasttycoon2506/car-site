a6d8a43d101abb4c20368c11727bd6d992a2dfe4
William Roberts <werobert@yahoo.com>
2025-07-02 10:38:34 -0400

    removed authResource carController

--- src/app/Http/Controllers/CarController.php
+++ src/app/Http/Controllers/CarController.php
@@ -9,12 +9,6 @@ use Illuminate\Support\Facades\Auth;
 
 class CarController extends Controller
 {
-    public function __construct()
-    {
-        $this->authorizeResource(Car::class, "car");
-    }
-
-
     public function index(Request $request)
     {
 
