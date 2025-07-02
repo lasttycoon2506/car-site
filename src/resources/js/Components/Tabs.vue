979c99912abdba171f000d39dc515682cdad0d66
William Roberts <werobert@yahoo.com>
2025-07-02 12:08:03 -0400

    tabs component

---
+++ src/resources/js/Components/Tabs.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!-- Tab headers -->
+        <div class="flex border-b mb-4">
+            <button v-for="tab in tabs" :key="tab" @click="activeTab = tab" :class="[
+                'px-4 py-2 font-semibold focus:outline-none',
+                activeTab === tab ? 'border-b-2 border-lime-500 text-lime-600' : 'text-gray-500'
+            ]">
+                {{ tab }}
+            </button>
+        </div>
+
+        <!-- Tab content -->
+        <div v-if="activeTab === 'UnreadNotifications'">
+            <!-- Place your notifications content here -->
+            <slot name="unreadNotifications" />
+        </div>
+        <div v-else-if="activeTab === 'ReadNotifications'">
+            <!-- Place your offers content here -->
+            <slot name="readNotifications" />
+        </div>
+        <div v-else>
+            <!-- Default or other tab content -->
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const tabs = ['UnreadNotifications', 'ReadNotifications']
+const activeTab = ref(tabs[0])
+</script>
--- src/resources/js/Pages/Notification/Index.vue
+++ src/resources/js/Pages/Notification/Index.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="mx-12 my-5">
         <h1 class="text-shadow-lg font-bold text-center text-4xl mt-5 mb-12">Your Notifications</h1>
-        <div v-for="notification in notifications.data" :key="notification.id">
-            <NotificationCard :notification="notification" />
-        </div>
+        <Tabs>
+            <template #unreadNotifications>
+                <div v-for="notification in notifications.data" :key="notification.id">
+                    <NotificationCard :notification="notification" />
+                </div>
+            </template>
+            <template #readNotifications>
+                <!-- Your offers list or component -->
+            </template>
+        </Tabs>
         <div v-if="notificationCount > 0" class="flex flex-col items-center my-10">
             <div class="mb-3">
                 <span class="font-semibold">{{ notificationCount }}</span> notifications
@@ -23,14 +30,17 @@ import type { PageProps } from "@/resources/types/pageProps";
 import { computed, ComputedRef } from "vue";
 import Pagination from "../../Components/Pagination.vue";
 import type { Link } from "@/resources/types/link";
+import Tabs from "../../Components/Tabs.vue";
 
 
 const page = usePage<PageProps>()
-defineProps<{
+const props = defineProps<{
     notifications: { data: Notification[], links: Link[] }
 }>()
 
 const notificationCount: ComputedRef<number> = computed(() =>
     page.props.user!.notificationCount
 )
+const unreadNotifications: ComputedRef<Notification[]> = computed(() => (
+    props.notifications.data.filter(element => !element.read_at)))
 </script>
